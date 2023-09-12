0757812342267b2d7b865ffab7636ed4c97aef1c
EugenFM <eugenfm22@gmail.com>
2023-09-12 14:32:20 -0500

    Two functions to fetch and display GitHub profile info

--- js/script.js
+++ js/script.js
@@ -3,3 +3,30 @@ const profileInfo = document.querySelector(".overview");
 
 const username = "EugenFM";
 
+// async function to fetch info from GitHub profile
+const githubApiInfo = async function () {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    // console.log(data);
+    displayInfo(data);
+}
+githubApiInfo();
+
+// function to display the fetched user information
+const displayInfo = async function (data) {
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("user-info");
+    newDiv.innerHTML = 
+    `<figure>  
+         <img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+    </div>`;
+
+    profileInfo.append(newDiv);
+}
+
